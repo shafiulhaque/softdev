c92b7e6359baed01bb496d317161c3f45f0330bc
shafiulhaque <shaque30@stuy.edu>
2022-10-17 14:08:09 -0400

    finihed work

---
+++ 12_flask-forms/app.py
@@ -0,0 +1,85 @@
+# Team Brown: Nakib Abedin, Eric Sohel, Shafiul Haque
+# SoftDev pd08
+# K11: Form(s) Like Voltron
+# 2022-10-13
+# time spent: 0.2hrs
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+
+#the conventional way:
+#from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
+
+mydict = {
+
+}
+
+def filereader():
+    file1 = open("names.csv",'r')
+    sha = file1.readlines()
+    for e in sha:
+        ar = e.split(',')
+        ar[2] = ar[2][0:len(ar[2])-1]
+        mydict[ar[0]] = ar[2]
+
+filereader()
+
+'''
+trioTASK:
+~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
+...read for understanding all of the code below.
+Some will work as written; other sections will not.
+TASK: Predict which...
+Devise some simple tests you can run to "take apart this engine," as it were.
+Execute your tests.
+Process results.
+
+PROTIP: Insert your own in-line comments
+ wherever they will help
+  your future self and/or current teammates
+   understand what is going on.
+'''
+
+@app.route("/") #, methods=['GET', 'POST'])
+def disp_loginpage():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.form)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    return render_template( 'login.html' )
+
+
+@app.route("/auth") # , methods=['GET', 'POST'])
+def authenticate():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.form)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    if (mydict[request.form.get('username')]==mydict[request.form.get('password')]):
+        return "RIGHT PASSWORD " + request.form.get('username') #response to a form submission
+    else:
+        return "WRONG PASSWORD"
+
+
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True
+    app.run()
